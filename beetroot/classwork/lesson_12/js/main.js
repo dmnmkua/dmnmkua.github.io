1979e77c252327296dd0d991218027c34e3244d0
dmnmkua <dmn.mk.ua@gmail.com>
2017-08-28 19:00:26 +0300

    change open top header in classwork lesson 12

--- beetroot/classwork/lesson_12/js/main.js
+++ beetroot/classwork/lesson_12/js/main.js
@@ -1,33 +1,12 @@
 window.onload = function() {
   var btn = document.getElementById('hide-btn');
-  var hideBlock = document.getElementById('hide-block');
-  var headerTop = document.getElementById('header-top');
-  var flag = false;
+  var wrapper = document.getElementById('wrapper');
 
 
   btn.addEventListener("click", function() {
-    if(flag) {
-      hideBlock.style.display = "none";
-      headerTop.style.padding = "0";
-      btn.style.bottom = "-25px";
-    }
-    else {
-      hideBlock.style.display = "flex";
-      headerTop.style.padding = "9px 0";
-      btn.style.bottom = "-35px";
-    }
 
-    flag = !flag;
+    wrapper.classList.toggle("wrapper--open");
+
   })
-  // btn.onclick = function() {
-  //
-  //   if(flag) {
-  //     hideBlock.style.display = "none";
-  //   }
-  //   else {
-  //     hideBlock.style.display = "block";
-  //   }
-  //
-  //   flag = !flag;
-  // }
+
 }
