717230bc90d1e615dde42da82401400a2ba83323
dmnmkua <dmn.mk.ua@gmail.com>
2017-08-28 15:59:09 +0300

    add button hidden in classwork lesson 12

---
+++ beetroot/classwork/lesson_12/js/main.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  var btn = document.getElementById('hide-btn');
+  var hideBlock = document.getElementById('hide-block');
+  var headerTop = document.getElementById('header-top');
+  var flag = false;
+
+
+  btn.addEventListener("click", function() {
+    if(flag) {
+      hideBlock.style.display = "none";
+      headerTop.style.padding = "0";
+      btn.style.bottom = "-25px";
+    }
+    else {
+      hideBlock.style.display = "block";
+      headerTop.style.padding = "9px 0";
+      btn.style.bottom = "-35px";
+    }
+
+    flag = !flag;
+  })
+  // btn.onclick = function() {
+  //
+  //   if(flag) {
+  //     hideBlock.style.display = "none";
+  //   }
+  //   else {
+  //     hideBlock.style.display = "block";
+  //   }
+  //
+  //   flag = !flag;
+  // }
+}
