a2d544cf4b21e7f0c80fb5c92288a0fcbca65783
dmnmkua <dmn.mk.ua@gmail.com>
2017-11-03 15:22:58 +0200

    delete beetroot in dmnmkua.github.io

--- beetroot/classwork/lesson_22/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.tab-btn').on('click', function(e) {
-    $('.tab-content').hide();
-    var anc = $(this).attr('href');
-    console.log(anc);
-    $(anc).show();
-  })
-})
